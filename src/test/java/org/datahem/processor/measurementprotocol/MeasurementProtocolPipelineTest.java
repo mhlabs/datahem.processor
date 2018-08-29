d348debd9332e96deda8f7cb80de5c349fbbeb5c
Robert Sahlin <robert.sahlin@mathem.se>
2018-08-29 12:22:23 +0200

    pipeline test running w dummy data

--- src/test/java/org/datahem/processor/measurementprotocol/MeasurementProtocolPipelineTest.java
+++ src/test/java/org/datahem/processor/measurementprotocol/MeasurementProtocolPipelineTest.java
@@ -33,16 +33,27 @@ import java.util.HashMap;
 import java.util.Arrays;
 import java.util.ArrayList;
 
-import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.ValidatesRunner;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
 
 import org.datahem.protobuf.collector.v1.CollectorPayloadEntityProto.CollectorPayloadEntity;
 import org.datahem.processor.measurementprotocol.utils.PayloadToMPEntityFn;
 import org.datahem.processor.measurementprotocol.utils.MPEntityToTableRowFn;
-import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
+import org.datahem.protobuf.measurementprotocol.v1.MPEntityProto.*;
+import org.datahem.protobuf.measurementprotocol.v1.MPEntityProto;
 
 import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
@@ -54,6 +65,8 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class MeasurementProtocolPipelineTest {
+
+	@Rule public transient TestPipeline p = TestPipeline.create();
 
 	private static String siteSearchPayload = "v=1&_v=j66&a=1140262547&t=pageview&_s=1&dl=https%3A%2F%2Fbeta.datahem.org%2Fsok%3Fq%3Dpasta%20knyten%26page%3D1%26pageSize%3D25&dp=%2Fvaror%2Fkott-o-chark&ul=sv&de=UTF-8&dt=Frukt%20%26%20Gr%C3%B6nt%20%7C%20Mathem&sd=24-bit&sr=1920x1200&vp=1292x1096&je=0&_u=aCDAAEAL~&jid=&gjid=&cid=1062063169.1517835391&uid=947563&tid=UA-7391864-18&_gid=616449507.1520411256&gtm=G2rP9BRHCJ&z=631938637&cd1=gold&cd2=family&cm1=25";
 	private static TableRow siteSearchTR = new TableRow().set("timestamp", 0).set("contributor_username", "user1");
@@ -67,8 +80,9 @@ public class MeasurementProtocolPipelineTest {
 				.build();
 	}
 
+	@Test
 	public void testMeasurementProtocolPipeline() throws Exception {
-		TestPipeline p = TestPipeline.create();
+		//@Rule public transient TestPipeline p = TestPipeline.create();
 
 		Map<String,String> headers = new HashMap<String, String>();
 		headers.put("X-AppEngine-Country","SE");
@@ -77,28 +91,34 @@ public class MeasurementProtocolPipelineTest {
 
 		CollectorPayloadEntity cpe = test(headers, siteSearchPayload);
 		
-		PCollection<TableRow> output =
-			p.apply(
+		PCollection<CollectorPayloadEntity> input = p.apply(
 				Create.of(
 					Arrays.asList(
-						cpe)))
-			.apply(new PayloadToMPEntityFn(
-				StaticValueProvider.of(".*"),//options.getSearchEnginesPattern(),
-				StaticValueProvider.of(".*"),//options.getIgnoredReferersPattern(), 
-				StaticValueProvider.of(".*"),//options.getSocialNetworksPattern(),
-				StaticValueProvider.of(".*"), // match all hostnames
-				StaticValueProvider.of("a^"), // match no user agent
-				StaticValueProvider.of(".*"),//options.getSiteSearchPattern(),
-				StaticValueProvider.of(".*")//options.getTimeZone()
-				))
-			.apply(new MPEntityToTableRowFn());
+						cpe)));
+		
+
+		PCollection<MPEntity> step1 =
+			input
+			.apply(ParDo.of(new PayloadToMPEntityFn(
+				StaticValueProvider.of(".*(www.google.|www.bing.|search.yahoo.).*"),//options.getSearchEnginesPattern(),
+				StaticValueProvider.of(".*(beta.datahem.org|www.datahem.org).*"),//options.getIgnoredReferersPattern(), 
+				StaticValueProvider.of(".*(facebook.|instagram.|pinterest.|youtube.|linkedin.|twitter.).*"),//options.getSocialNetworksPattern(),
+				StaticValueProvider.of(".*(beta.datahem.org|www.datahem.org).*"), // match all hostnames
+				StaticValueProvider.of(".*(^$|bot|spider|crawler).*"), // match no user agent
+				StaticValueProvider.of(".*q=(([^&#]*)|&|#|$)"),//options.getSiteSearchPattern(),
+				StaticValueProvider.of("Europe/Stockholm")//options.getTimeZone()
+				)));
+		
+		PCollection<TableRow> output =
+			step1
+				.apply(ParDo.of(new MPEntityToTableRowFn()));
 
       
        PAssert.that(output)
         .containsInAnyOrder(
             Arrays.asList(siteSearchTR));
 
-    p.run().waitUntilFinish();
+    p.run();
 
     }
 }
