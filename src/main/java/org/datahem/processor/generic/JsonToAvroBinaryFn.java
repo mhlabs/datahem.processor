2c3b606381461fab130fc5397fc9a26059a831e7
Robert Sahlin <robert.sahlin@mathem.se>
2018-10-15 15:49:12 +0200

    avro serializer

---
+++ src/main/java/org/datahem/processor/generic/JsonToAvroBinaryFn.java
@@ -0,0 +1,78 @@
+package org.datahem.processor.generic;
+
+/*-
+ * ========================LICENSE_START=================================
+ * DataHem
+ * %%
+ * Copyright (C) 2018 Robert Sahlin and MatHem Sverige AB
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =========================LICENSE_END==================================
+ */
+
+
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+import org.datahem.avro.message.Converters;
+import org.datahem.avro.message.DatastoreCache;
+import org.datahem.avro.message.DynamicBinaryMessageDecoder;
+import org.apache.avro.generic.GenericData.Record;
+import org.apache.avro.generic.GenericData;
+//import org.apache.beam.sdk.options.ValueProvider;
+import org.joda.time.Instant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class JsonToAvroBinaryFn extends DoFn<PubsubMessage, PubsubMessage> {
+		private static final Logger LOG = LoggerFactory.getLogger(JsonToAvroBinaryFn.class);
+		private DatastoreCache cache;
+		private DynamicBinaryMessageDecoder<Record> decoder;
+		
+		//public JsonToAvroBinaryFn() {}
+		
+		@Setup
+			public void setup(StartBundleContext c) throws Exception {
+				cache = new DatastoreCache();
+				cache.addSchema(SCHEMA_V1);
+				decoder = new DynamicBinaryMessageDecoder<>(GenericData.get(), SCHEMA_V1, cache);
+			}
+		
+		@ProcessElement
+			public void processElement(ProcessContext c) throws Exception {
+				PubsubMessage received = c.element();
+
+				try{
+					byte[] payload = Converters.jsonToAvroBinary(c.getPayload(), SCHEMA_V1);
+					Map<String,String> attributes = 
+								ImmutableMap.<String, String>builder()
+									.put("timestamp", Long.toString(Instant.now().getMillis()))
+									.put("stream", name)
+									.put("recordNamespace", recordNamespace)
+									.put("recordName", recordName)
+									.put("uuid", UUID.randomUUID().toString())
+									.build();
+	
+					PubsubMessage pubsubMessage = new PubsubMessage(payload, attributes);
+					c.output(pubsubMessage);
+				}catch(Exception e){
+					LOG.error(e.toString());
+				}
+    		}
+  }
