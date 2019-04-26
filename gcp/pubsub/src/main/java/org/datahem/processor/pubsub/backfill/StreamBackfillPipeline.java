06d0418db32c0e96c5bb0052df15be68ed17bdeb
Robert Sahlin <robert.sahlin@mathem.se>
2019-04-26 10:13:01 +0200

    backfill compile

--- gcp/pubsub/src/main/java/org/datahem/processor/pubsub/backfill/StreamBackfillPipeline.java
+++ gcp/pubsub/src/main/java/org/datahem/processor/pubsub/backfill/StreamBackfillPipeline.java
@@ -1,7 +1,33 @@
 
 package org.datahem.processor.pubsub.backfill;
 
-import org.datahem.processor.utils.BackupToByteArrayFn;
+/*-
+ * ========================LICENSE_START=================================
+ * DataHem
+ * %%
+ * Copyright (C) 2018 - 2019 Robert Sahlin and MatHem Sverige AB
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * =========================LICENSE_END==================================
+ */
+
+//import org.datahem.processor.utils.BackupToByteArrayFn;
 
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
@@ -11,10 +37,14 @@ import com.google.api.services.bigquery.model.TimePartitioning;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.util.List;
+import java.util.HashMap;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryHelpers;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
+import org.apache.beam.sdk.io.gcp.pubsub.PubsubMessage;
+
 import org.apache.beam.sdk.metrics.Counter;
 import org.apache.beam.sdk.metrics.Metrics;
 import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
@@ -80,13 +110,13 @@ public class StreamBackfillPipeline {
     			.usingStandardSql())
     			//.withoutValidation())
     	.apply("TableRow to PubSubMessage", 
-			ParDo.of(new DoFn<TableRow,PubSubMessage>() {
+			ParDo.of(new DoFn<TableRow,PubsubMessage>() {
 	      		@ProcessElement
 	      		public void processElement(ProcessContext c)  {
                     TableRow row = c.element();
 			        byte[] payload = (byte[]) row.get("data");
                     HashMap<String, String> attributes = (HashMap<String, String>) row.get("attributes");
-                    PubsubMessage pubSubMessage = PubsubMessage(payload, attributes); 
+                    PubsubMessage pubSubMessage = new PubsubMessage(payload, attributes); 
                     c.output(pubSubMessage);
          }}))
          .apply("Write to pubsub",
