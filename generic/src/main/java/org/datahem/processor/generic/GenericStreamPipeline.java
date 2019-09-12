7fd88e9be525e5a558fd4d13951d29b2371f4a1d
Robert Sahlin <robert.sahlin@mathem.se>
2019-09-12 15:26:56 +0200

    backup clustering and pom clean up and beam v upgrade to 2.15.0

--- generic/src/main/java/org/datahem/processor/generic/GenericStreamPipeline.java
+++ generic/src/main/java/org/datahem/processor/generic/GenericStreamPipeline.java
@@ -253,11 +253,16 @@ public static ProtoDescriptor getProtoDescriptorFromCloudStorage(
                 }
 				try{
                     DynamicMessage.Builder builder = DynamicMessage.newBuilder(messageDescriptor);
-                    try{
-                        JsonFormat.parser().merge(payload, builder);
-                    }catch(InvalidProtocolBufferException e){
-                        LOG.error("Unknown fields in message, doesn't match current schema " + descriptorFullName.get(), e);
-                        JsonFormat.parser().ignoringUnknownFields().merge(payload, builder);
+                    if(messageDescriptor.findFieldByName("_DATA") != null){
+                        try{
+                            JsonFormat.parser().merge(payload, builder);
+                        }catch(InvalidProtocolBufferException e){
+                            LOG.error("Unknown fields in message, doesn't match current schema " + descriptorFullName.get(), e);
+                            builder.clear();
+                            JsonFormat.parser().ignoringUnknownFields().merge(payload, builder);
+                        }
+                    } else{
+                        builder.setField(messageDescriptor.findFieldByName("_DATA"), payload);
                     }
 					try{
                         attributes.entrySet().forEach(attribute -> {
